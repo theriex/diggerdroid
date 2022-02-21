685a768deba5f5d66b8b0bf57dda618aa42a4ade
Eric Parker <theriex@gmail.com>
2022-02-21 09:24:10 -0500

    Basic WebView display

--- app/src/main/java/com/diggerhub/digger/MainActivity.kt
+++ app/src/main/java/com/diggerhub/digger/MainActivity.kt
@@ -2,10 +2,14 @@ package com.diggerhub.digger
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val myWebView: WebView = findViewById(R.id.webview)
+        myWebView.getSettings().setJavaScriptEnabled(true);
+        myWebView.loadUrl("https://www.epinova.com")
     }
 }
