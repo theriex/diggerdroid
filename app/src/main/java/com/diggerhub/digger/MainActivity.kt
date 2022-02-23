07405cf9e30101c36ec3b368230f590ea6775d3e
Eric Parker <theriex@gmail.com>
2022-02-23 09:16:19 -0500

    Initial android script load

--- app/src/main/java/com/diggerhub/digger/MainActivity.kt
+++ app/src/main/java/com/diggerhub/digger/MainActivity.kt
@@ -3,13 +3,34 @@ package com.diggerhub.digger
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.webkit.WebView
+import android.webkit.WebResourceRequest
+import android.webkit.WebResourceResponse
+import androidx.webkit.WebViewAssetLoader
+import android.webkit.WebViewClient
+import android.webkit.WebChromeClient
+import android.webkit.ConsoleMessage
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val myWebView: WebView = findViewById(R.id.webview)
-        myWebView.getSettings().setJavaScriptEnabled(true);
-        myWebView.loadUrl("https://www.epinova.com")
-    }
+        val dwv: WebView = findViewById(R.id.webview)
+        val assetLoader = WebViewAssetLoader.Builder()
+            .addPathHandler("/assets/", WebViewAssetLoader.AssetsPathHandler(this))
+            .build();
+        dwv.webViewClient = object : WebViewClient() {
+            override fun shouldInterceptRequest(
+                view: WebView,
+                request: WebResourceRequest
+            ): WebResourceResponse? {
+                return assetLoader.shouldInterceptRequest(request.url);
+            } }
+        dwv.webChromeClient = object : WebChromeClient() {
+            override fun onConsoleMessage(msg: ConsoleMessage): Boolean {
+                Log.d("Digger", "${msg.message()} -- From line " +
+                      "${msg.lineNumber()} of ${msg.sourceId()}")
+                return true } }
+        dwv.getSettings().setJavaScriptEnabled(true)
+        dwv.loadUrl("https://appassets.androidplatform.net/assets/index.html") }
 }
---
+++ ref/makelinks.js
@@ -0,0 +1,73 @@
+/*jslint node, white */
+
+//Link the digger webapp src files into app/src/main/assets
+//usage:
+//  node makelinks.js
+//or
+//  node makelinks.js delete
+//to get rid of the created links.  The digger project must be available as
+//a sibling project off the same parent directory as diggerdroid.
+
+var linker = (function () {
+    "use strict";
+
+    var fs = require("fs");
+    var ws = {linkdirs:["", "css", "img", "js", "js/amd", "docs"]};
+
+
+    function makeWorkingSetRoots () {
+        var dn = __dirname;
+        ws.lnkr = dn.slice(0, dn.lastIndexOf("/"));
+        ws.digr = ws.lnkr.slice(0, ws.lnkr.lastIndexOf("/") + 1) +
+            "digger/docroot/";
+        ws.lnkr += "/app/src/main/assets/";
+    }
+
+
+    function jslf (obj, method, ...args) {
+        return obj[method].apply(obj, args);
+    }
+
+
+    function checkLink (cmd, relpath, fname) {
+        var hfp = ws.lnkr + relpath + "/" + fname;
+        var dfp = ws.digr + relpath + "/" + fname;
+        if(fname.endsWith("~")) { return; }
+        if(!jslf(fs, "existsSync", hfp)) {
+            if(cmd === "create") {
+                fs.symlink(dfp, hfp, function (err) {
+                    if(err) { throw err; }
+                    console.log("created " + hfp); }); }
+            else {
+                console.log("missing " + hfp); } }
+        else {
+            if(cmd === "delete") {
+                fs.unlink(hfp, function (err) {
+                    if(err) { throw err; }
+                    console.log("removed " + hfp); }); }
+            else {
+                console.log(" exists " + hfp); } }
+    }
+
+
+    function traverseLinks (cmd) {
+        console.log("Command: " + cmd);
+        makeWorkingSetRoots();
+        console.log("Linking " + ws.digr + " files to " + ws.lnkr);
+        ws.linkdirs.forEach(function (relpath) {
+            var dir = ws.digr + relpath;
+            var options = {encoding:"utf8", withFileTypes:true};
+            fs.readdir(dir, options, function (err, dirents) {
+                if(err) { throw err; }
+                dirents.forEach(function (dirent) {
+                    if(dirent.isFile()) {
+                        checkLink(cmd, relpath, dirent.name); } }); }); });
+    }
+
+
+    return {
+        run: function () { traverseLinks(process.argv[2] || "create"); }
+    };
+}());
+
+linker.run();
